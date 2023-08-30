f458f26cddafc92be43f5cef2604b5191f184b39
chezzandyto <andres_toapanta@hotmail.com>
2023-08-29 20:11:38 -0500

    comment added

--- Corporative/dml/01_insert_quality.sql
+++ Corporative/dml/01_insert_quality.sql
@@ -1,3 +1,4 @@
+/* Quality Catalogue*/
 INSERT INTO corporative.corpt_quality (name, status, created_by_user) VALUES ('Alta', '1', '948dfe9e-44c8-45a0-b358-f31bb29e62e1');
 INSERT INTO corporative.corpt_quality (name, status, created_by_user) VALUES ('Media', '1', '948dfe9e-44c8-45a0-b358-f31bb29e62e1');
 INSERT INTO corporative.corpt_quality (name, status, created_by_user) VALUES ('Baja', '1', '948dfe9e-44c8-45a0-b358-f31bb29e62e1');
